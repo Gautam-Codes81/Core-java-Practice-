695d3c7dfb4b66ae7510f4e3b8cb8b05a27df7cf
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-02-12 05:16:41 +1100

    Add files via upload

---
+++ method_overloding.java
@@ -0,0 +1,64 @@
+public class method_overloding {
+
+  // public static void main(String[] args) {
+
+  /*
+   * static void telljoke() {
+   * System.out.println("I inverted a new word!" + "plagiarism");
+   * }
+   * 
+   * public static void main(String[] args) {
+   * telljoke();
+   * }
+   */
+
+  // question 2
+  /*
+   * static void change(int a) {
+   * a = 98;
+   * }
+   * 
+   * static void change2(int[] arr) {
+   * arr[0] = 98;
+   * }
+   * 
+   * public static void main(String[] args) {
+   * int[] marks = { 52, 73, 77, 89, 98, 94 };
+   * change2(marks);
+   * System.out.println("the value of x after running change is:" + marks[0]);
+   * 
+   * 
+   * }
+   */
+
+  /*static void foo() {
+    System.out.println("Good morning bro!");
+
+  }
+
+  static void foo(int a) {
+    System.err.println("good morning" + a + ("bro"));
+
+  }
+
+  static void foo(int a, int b) {
+    System.out.println("good morning " + a + "bro!");
+    System.out.println("good morning" + b + "bro!");
+    foo();
+
+  }
+
+  static void foo(int a, int b, int c) {
+    System.out.println("good morning " + a + "bro!");
+    System.out.println("good morning" + b + "bro");
+    System.out.println("good morning" + c + ("bro!"));
+
+  }
+
+} 
+  */
+
+  
+
+  
+}
