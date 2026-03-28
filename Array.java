2270cabb01afd7f0c384e382c91a815f89bc9f43
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-03-28 22:41:31 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,44 @@
+import javax.swing.plaf.synth.SynthListUI;
+
+public class Array {
+
+    public static void main(String[] args) {
+       /*  int num [] = {4,5,6,78,2};
+        for(int i=0; i<=4; i++){
+            System.out.println(num[i]);
+        } */
+
+           /*  int number [] = {2,3,44,67,89,69,98};
+            for(int i = 0; i<=6; i++){
+                System.out.println(number[i]);
+            } */
+
+              /*   int gk [] = {27,45,67,35,67,75,76,56,46,45};
+                for(int i = 0; i<=gk.length; i++){
+                    System.out.println(gk[i]);
+                } */
+
+ /*  int arry [] = {23,45,4,6};
+  for (int i = 0; i <= 4; i++) {
+    System.out.println(arry[i]);
+    
+  } */
+ /*int array [] = {2,46,75,8,67,567,8,6,9};
+ for(int i=0; i<=array.length; i++){
+    System.out.println(array[i]); 
+ } */
+
+int num [] = new int[4];
+num[0] = 5;
+num[1] = 4;
+num[2] = 6;
+num[3] = 7;
+// num[4] = 8;
+for(int i = 0; i<=3; i++){
+    System.out.println(num[i]);
+}
+                    
+
+    }
+
+}
