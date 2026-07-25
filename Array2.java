8670585b0b7ce2a4777b317101c6351c31745cde
Gautam-Codes81 <gautamkushwaha8115@gmail.com>
2026-07-25 19:58:51 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,45 @@
+public class Array2 {
+    
+public static void main(String[] args) {
+   // int num [] = {2,4,5,6,7,8};
+    // System.out.println(num[2]);
+    // System.out.println(num[4]);
+    // for(int i = 0; i<=5; i++){
+    //     System.out.println(num[i]);
+    // }
+
+    // int gk [] = {3,5,6,7,89,98};
+    // for(int i = 0; i<=5; i++){
+    //     System.out.println(gk[i]);
+    // }
+  
+    // int gk [] = new int[5];
+    // gk[0] = 9;
+    // gk[1] = 10;
+    // gk[2] = 12;
+    // gk[3] = 14;
+    // gk[4] = 16;
+
+    // for(int i = 0; i<=4; i++){
+    //     System.out.println(gk[i]);
+    // }
+
+    // int num [] = {2,4,5,6,7,8,9,};
+    // // System.out.println(num[3]);
+    // for(int i = 0; i<=6; i++){
+    //     System.out.println(num[i]);
+    // }
+
+    int num [] = new int[5];
+     num[0] = 5;
+     num[1] = 6;
+     num[2] = 15;
+     num[3] = 17;
+     for(int i = 0; i<=3; i++){
+        System.out.println(num[i]);
+     }
+
+
+
+}
+}
